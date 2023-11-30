9b7deeef584418468644e351284e935dbabcbe27
Jeremiah Dy <97951461+jeremiah-dy@users.noreply.github.com>
2023-11-30 10:47:07 -1000

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import unittest
 from product_database import make_product_database
 
-class product_database_test(unittest.TestCase):
-    def test_product(self):
-        make_product_database()
+# class product_database_test(unittest.TestCase):
+#     def test_product(self):
+#         make_product_database()
 
 
-#class vendor_database_test(unittest.TestCase):
-    #def 
+# class vendor_database_test(unittest.TestCase):
+#     def test_vendor(self):
+#         make_vendor_database()
 
 if __name__ == "__main__":
     unittest.main()
