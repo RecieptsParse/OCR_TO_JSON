79b170e2d77e7916235f046812ff1621e781721b
amandanitta <amandanitta458@gmail.com>
2023-12-03 20:03:19 -1000

    update test.py

--- test.py
+++ test.py
@@ -8,49 +8,47 @@ class product_database_test(unittest.TestCase):
      def test_product(self):
          make_product_database()
      
-     def load_data_from_pk1_file(self,file_path):
+     def load_data_from_pk1_file_product(self,file_path):
         with open(file_path, 'rb') as f:
             data = pickle.load(f)
         return data
      
-     def test_load_data_from_pk1_file(self):
+     def test_load_data_from_pk1_file_product(self):
         test_instance = product_database_test()
-        sample_data = {'key': 'value'}
+        sample_data_product = {'key': 'value'}
         with open('product_mapping.pk1', 'wb') as f:
-            pickle.dump(sample_data, f)
-        loaded_data = test_instance.load_data_from_pk1_file('product_mapping.pk1')
+            pickle.dump(sample_data_product, f)
+        loaded_data_product = test_instance.load_data_from_pk1_file_product('product_mapping.pk1')
 
-        self.assertEqual(loaded_data, sample_data)
-        expected_entries = len(sample_data)
-        actual_entries = len(loaded_data)
+        self.assertEqual(loaded_data_product, sample_data_product)
+        expected_entries = len(sample_data_product)
+        actual_entries = len(loaded_data_product)
         self.assertEqual(actual_entries, expected_entries, 'failed actual entries')
         print('here')
         os.remove('product_mapping.pk1')
 
 
-# class vendor_database_test(unittest.TestCase):
-#      def test_vendor(self):
-#          make_vendor_database()
-#          return 
-#      def load_data_from_pk1_file(self,file_path):
-#         with open(file_path, 'rb') as f:
-#             data = pickle.load(f)
-#         return data
+class vendor_database_test(unittest.TestCase):
+     def test_vendor(self):
+         make_vendor_database()
+         return 
+     def load_data_from_pk1_file_vendor(self,file_path):
+        with open(file_path, 'rb') as f:
+            data = pickle.load(f)
+        return data
      
-#      def test_load_data_from_pk1_file(self):
-#         test_instance = vendor_database_test()
-#         sample_data = {'key': 'value'}
-#         with open('vendor_mapping.pk1', 'wb') as f:
-#             pickle.dump(sample_data, f)
-#         loaded_data = test_instance.load_data_from_pk1_file('vendor_mapping.pk1')
+     def test_load_data_from_pk1_file_vendor(self):
+        test_instance = vendor_database_test()
+        sample_data_vendor = {'key': 'value'}
+        with open('vendor_mapping.pk1', 'wb') as f:
+            pickle.dump(sample_data_vendor, f)
+        loaded_data_vendor = test_instance.load_data_from_pk1_file_vendor('vendor_mapping.pk1')
 
-#         self.assertEqual(loaded_data, sample_data, 'failed equal data')
-#         print('Equal Data')
-#         expected_entries = len(sample_data)
-#         actual_entries = len(loaded_data)
-#         # self.assertEqual(actual_entries, expected_entries,'failed equal vendor')
-#         print('here')
-#         os.remove('vendor_mapping.pk1')
+        self.assertEqual(loaded_data_vendor, sample_data_vendor, 'failed equal data')
+        expected_entries = len(sample_data_vendor)
+        actual_entries = len(loaded_data_vendor)
+        self.assertEqual(actual_entries, expected_entries,'failed equal vendor')
+        os.remove('vendor_mapping.pk1')
 
 if __name__ == "__main__":
     unittest.main()
