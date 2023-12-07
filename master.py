1fb288de304a87686b0d1bd849f6d3578bafd02f
amandanitta <amandanitta458@gmail.com>
2023-12-06 19:46:33 -1000

    revised to write cat to JSON

--- master.py
+++ master.py
@@ -96,11 +96,12 @@ def read_one(json_object):
             query_string += f'{dining_option} '
             for j in range(len(one_reciept['ITEMS'])):
                 items = one_reciept['ITEMS'][j]['unabbreviatedDescription']
-                query_string += f' | {items} |'
+                query_string += f' {items} '
 
             top_vendor = search.query_classification(query_string, 5, "vendor")
             file.write(f'- {i} Vendor Prediction {top_vendor}\n')
             file.write(f'Query: {query_string}\n')
+            one_reciept['vendor_cat'] = top_vendor
             items_for_receipt = one_reciept['ITEMS']
             for j in range(len(one_reciept['ITEMS'])):
                 product_query = items_for_receipt[j]['unabbreviatedDescription']
@@ -108,13 +109,12 @@ def read_one(json_object):
                 product_query += f' {descr} {merchant_name}' 
                 top_product = search.query_classification(product_query, 10, "product")
                 file.write(f' {j} Item: {product_query} category: {top_product}\n')
+                items_for_receipt[j]['item_cat'] = top_product
 
-# write to csv
+# write to JSON only
 """DID NOT TEST YET"""
 # def read_one(json_object):
-#     with open('output.csv', 'a',newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['Number','Vendor','Item'])
+#     with open('output.txt', 'a') as file:
 #         for i in range(len(json_object)):
 #             query_string = ""
 #             one_reciept = json_object[i]
@@ -124,18 +124,17 @@ def read_one(json_object):
 #             query_string += f'{dining_option} '
 #             for j in range(len(one_reciept['ITEMS'])):
 #                 items = one_reciept['ITEMS'][j]['unabbreviatedDescription']
-#                 query_string += f'{items} '
+#                 query_string += f' {items} '
 
 #             top_vendor = search.query_classification(query_string, 5, "vendor")
+#             one_reciept['vendor_cat'] = top_vendor
 #             items_for_receipt = one_reciept['ITEMS']
 #             for j in range(len(one_reciept['ITEMS'])):
-#                 arr = [i,top_vendor]
 #                 product_query = items_for_receipt[j]['unabbreviatedDescription']
 #                 descr = items_for_receipt[j]['description']
 #                 product_query += f' {descr} {merchant_name}' 
-#                 top_product = search.query_classification(product_query, 4, "product")
-#                 arr.append(top_product)
-#                 writer.writerow(arr)     
+#                 top_product = search.query_classification(product_query, 10, "product")
+#                 items_for_receipt[j]['item_cat'] = top_product
 
 
 # {Merchant} {First Item} {diningOptions} {SecondItem}
