17dc335c457aa8733bb51a4151775a496e973d00
Jeremiah Dy <97951461+jeremiah-dy@users.noreply.github.com>
2023-12-07 23:11:28 -1000

    Updated master.py with skip functionality

--- master.py
+++ master.py
@@ -7,7 +7,6 @@ import json
 import pandas as pd
 import csv
 
-
 '''
 saves faiss index and mapping to directory 
 model for embeddings & descriptions can be updated in config.py
@@ -29,22 +28,29 @@ fewshot_prompt = convert.make_fewshot_prompt(receiptParser.get_format_instructio
 model = convert.make_model(model="gpt-4-1106-preview", temperature=1.00, openai_api_key=openai_api_key)
 chain = convert.make_chain(fewshot_prompt, model, receiptParser)
 
+# If a sys_arg was passed (for number of files to skip), note it down
+skip_num_files = 0
+if((len(sys.argv) - 1) > 0):
+    skip_num_files = int(sys.argv[1])
+
 # Iterate through receipts_folder, and convert each receipt to a JSON object
-i=1
+i=0
 for filename in os.listdir(receipts_folder):
+    i+= 1
+    if i < skip_num_files:
+        continue
     if filename.endswith('.txt'):
         with open(os.path.join(receipts_folder, filename)) as f:
             data = f.read()
-            print('\n',i)
+            print('\n')
             print(f'reading from {filename}')
             response = chain.invoke({"input": data})
             # with 'a' in open() it will create or append to current file
             with open('receipts_json.json', 'a') as fp: # Write each JSON object as it gets parsed
                     # write each receipt JSON on a new line
                     fp.write(json.dumps({"ReceiptInfo": json.loads(response.model_dump_json())}) + "\n")
-    i+=1
-    print('Done Converting Reciept Text')
-
+    print(f'Done converting {filename}, JSON located at line {i}')    
+    
 """
 NEED TO TEST
 """
